6055baa94b396d0d32e1f789132191b9d587d1f1
BTSTeam <bts@zitko.co.uk>
2025-08-14 11:13:07 +0100

    Update pkce.ts

--- lib/pkce.ts
+++ lib/pkce.ts
@@ -1,17 +1,13 @@
-import crypto from 'crypto'
+import { createHash, randomBytes } from 'crypto';
 
-export function base64URLEncode(buffer: Buffer) {
-  return buffer.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '')
+const b64url = (buf: Buffer) =>
+  buf.toString('base64').replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/,'');
+
+export function generateVerifier(): string {
+  return b64url(randomBytes(32));
 }
 
-export function sha256(buffer: Buffer) {
-  return crypto.createHash('sha256').update(buffer).digest()
-}
-
-export function generateVerifier() {
-  return base64URLEncode(crypto.randomBytes(32))
-}
-
-export function challengeFromVerifier(v: string) {
-  return base64URLEncode(sha256(Buffer.from(v)))
+export function challengeFromVerifier(verifier: string): string {
+  const hash = createHash('sha256').update(verifier).digest();
+  return b64url(hash);
 }
