a26aa1010b70a5b3a1b2980eacdc9a71efc7ae26
BTSTeam <bts@zitko.co.uk>
2025-10-03 17:08:39 +0100

    Delete app directory

--- app/_components/HideOnLogin.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import { usePathname } from 'next/navigation'
-import type { ReactNode } from 'react'
-
-export default function HideOnLogin({ children }: { children: ReactNode }) {
-  const pathname = usePathname()
-  if (pathname === '/login') return null
-  return <>{children}</>
-}
