f13f3d609033257f45761bb68417c34b8494c360
BTSTeam <bts@zitko.co.uk>
2025-10-20 15:20:09 +0100

    Delete app/api/apollo/oauth directory

--- app/api/apollo/oauth/status/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from 'next/server'
-import { ensureApolloToken } from '@/lib/apolloRefresh'
-
-export async function GET() {
-  const token = await ensureApolloToken().catch(() => null)
-  return NextResponse.json({ connected: !!token })
-}
