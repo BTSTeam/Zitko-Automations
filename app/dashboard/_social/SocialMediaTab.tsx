dcd5a8872d3eb3becd5135d403c40511deac7f2b
BTSTeam <bts@zitko.co.uk>
2025-10-27 13:43:59 +0000

    Update SocialMediaTab.tsx

--- app/dashboard/_social/SocialMediaTab.tsx
+++ app/dashboard/_social/SocialMediaTab.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useMemo, useRef, useState } from 'react'
-import Recorder from '../_components/Recorder'
+import Recorder from '../../_components/Recorder'
 import * as htmlToImage from 'html-to-image'
 
 type SocialMode = 'jobPosts' | 'generalPosts'
