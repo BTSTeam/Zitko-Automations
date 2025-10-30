98b4a17804ff1dfe0b09381d36ab6d9d281d7245
BTSTeam <bts@zitko.co.uk>
2025-10-30 12:39:23 +0000

    Update SourceTab.tsx

--- app/dashboard/_source/SourceTab.tsx
+++ app/dashboard/_source/SourceTab.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from 'react'
 
-type SourceMode = 'people' | 'companies'
+type SourceMode = 'candidate' | 'companies'
 
 type EmploymentItem = {
   organization_name: string | null
