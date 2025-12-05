062b6fdc2588a61c8768f1fabcb4148cf0499f29
BTSTeam <bts@zitko.co.uk>
2025-12-05 13:15:15 +0000

    Update ClientShell.tsx

--- app/dashboard/ClientShell.tsx
+++ app/dashboard/ClientShell.tsx
@@ -23,6 +23,7 @@ type SocialMode = 'jobPosts' | 'jobZone'
 // Toggles
 const DISABLE_SOURCING = false
 const DISABLE_SOCIAL   = false
+const DISABLE_DATA   = true
 
 export default function ClientShell(): JSX.Element {
   const [tab, setTab] = useState<TabKey>('match')
