60887717690c0cf2d1fe08f1521d0f14faf32919
BTSTeam <bts@zitko.co.uk>
2025-12-11 17:17:20 +0000

    Update WooTab.tsx

--- app/dashboard/_match/WooTab.tsx
+++ app/dashboard/_match/WooTab.tsx
@@ -13,7 +13,7 @@ export default function WooTab() {
         src="https://agents.woo.io/ai-sourcer-agent"
         style={{
           width: '111%',                
-          height: 'calc(100vh - 150px)',
+          height: 'calc(100vh - 180px)',
           transform: 'scale(0.9)',      
           transformOrigin: '0 0',       
           border: '0'
